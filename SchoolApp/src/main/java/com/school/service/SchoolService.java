d55cbefb008ca9770ca534cb62f081b63cd85f38
Abu Talha Siddiqi <asiddi08@in.ibm.com>
2019-03-28 12:22:27 +0530

    1st commit

---
+++ SchoolApp/src/main/java/com/school/service/SchoolService.java
@@ -0,0 +1,9 @@
+package com.school.service;
+
+import com.school.response.model.SchoolModel;
+
+public interface SchoolService {
+	
+	public  SchoolModel getStudentByschoolName(String schoolName);
+
+}
